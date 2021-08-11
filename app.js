5429be7846fb03483e96e4180c0cacc7793529f0
YOUR NAME <62856817+AllisOrder@users.noreply.github.com>
2021-08-11 10:46:16 -0400

    First Push

---
+++ app.js
@@ -0,0 +1,20 @@
+var generateS = (numS) => {
+  for (let i = 0; i <= numS; i++) {
+    // create a new html element
+    let square = $("<div>").addClass("square");
+    // generate random colors
+    square.css('background-color', generateC())
+    // add the num of each square
+    // square.text(i)
+    // have the class of .suare
+    // append it to the body
+    $("body").append(square);
+  }
+
+};
+var generateC = () => {
+  var red = Math.floor(Math.random() * 255);
+  var green = Math.floor(Math.random() * 255);
+  var blue = Math.floor(Math.random() * 255);
+  return 'rgb(' +red +', '+ green+',' +blue+')'
+};
