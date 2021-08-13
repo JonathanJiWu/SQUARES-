dffe12bec677ccde99368cb28d486f54c8563c3d
YOUR NAME <62856817+AllisOrder@users.noreply.github.com>
2021-08-12 22:17:14 -0400

    add pink

--- app.js
+++ app.js
@@ -18,3 +18,24 @@ var generateC = () => {
   var blue = Math.floor(Math.random() * 255);
   return 'rgb(' +red +', '+ green+',' +blue+')'
 };
+// pink squares
+var generateSP = (numS) => {
+  for (let i = 0; i <= numS; i++) {
+    // create a new html element
+    let square = $("<div>").addClass("square");
+    // generate random colors
+    square.css('background-color', generateCP())
+    // add the num of each square
+    // square.text(i)
+    // have the class of .suare
+    // append it to the body
+    $("body").append(square);
+  }
+
+};
+var generateCP = () => {
+  var red = Math.floor(Math.random() * 255);
+  var green = Math.floor(Math.random() * 255);
+  var blue = Math.floor(Math.random() * 255);
+  return 'rgb(' +red +', '+ green+',' +blue+')'
+};
